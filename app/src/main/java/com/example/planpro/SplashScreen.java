5af3e2c5a02b2de999a00e9c3eb3559993ab1bcb
Rima AlSadhan <44276239+RimaSadh@users.noreply.github.com>
2020-03-23 16:16:00 +0300

    fix

---
+++ app/src/main/java/com/example/planpro/SplashScreen.java
@@ -0,0 +1,63 @@
+package com.example.planpro;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreen extends AppCompatActivity implements View.OnClickListener{
+
+
+    Timer timer;
+    boolean click = false;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+        ConstraintLayout splashlin = findViewById(R.id.splash);
+        splashlin.setOnClickListener(this);
+        timer = new Timer();
+
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+
+                if (click)
+                    return;
+
+                    Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+
+            }
+        }, 2000);
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.splash:
+
+                    //User is logged in
+                    Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+
+                break;
+        }
+
+    }
+
+}
